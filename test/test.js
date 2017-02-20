df62f12e9e966f727eda090769b73523bbe5d7e3
mtimofiiv <github@fiiv.io>
2017-02-20 18:43:44 +0100

    almost done methods, will need a lot of love afterwards to implement real life

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@
 require('dotenv').config()
 
 const expect = require('chai').expect
+const mongo = require('mongodb')
 const _Module = require('../index')
 
 const TEST_MONGO_URI = process.env.MONGO_URI || 'mongodb://localhost/mongotest'
@@ -19,22 +20,23 @@ describe('phobos.js mongodb', () => {
   }
 
   before(done => {
-    DB.init({ connection: { uri: TEST_PG_URI } }).then(client => {
+    DB.init({ connection: { uri: TEST_MONGO_URI } }).then(client => {
       store = client
-      done()
+
+      Model.attribute('username', { type: String })
+
+      attachListener(() => {})
+
+      Model.init(store).then(() => {
+        done()
+        // const Billy = new Model({ username: 'Billy' })
+
+        // Billy.save().then(result => {
+        //   billyId = result.id
+        //   done()
+        // })
+      })
     })
-    // Model.attribute('username', { type: 'varchar(30)' })
-    //
-    // attachListener(() => {})
-    //
-    // Model.init(store).then(() => {
-    //   const Billy = new Model({ username: 'Billy' })
-    //
-    //   Billy.save().then(result => {
-    //     billyId = result.id
-    //     done()
-    //   })
-    // })
   })
 
   // after(done => {
@@ -48,98 +50,115 @@ describe('phobos.js mongodb', () => {
   //   })
   // })
   //
-  // it('initializes a pg.Pool object', () => {
-  //   expect(store).to.be.instanceof(pg.Pool)
-  // })
-  //
-  // it('model extends PhobosPostgresModel', done => {
-  //   const inst = new Model()
-  //
-  //   expect(inst).to.be.instanceof(_Module.Model)
-  //   expect(inst.table).to.equal('models')
-  //
-  //   done()
-  // })
-  //
-  // it('properly serializes into an object via toObject() and stores changes in _dirty', () => {
-  //   const inst = new Model()
-  //
-  //   inst.username = 'wutwut'
-  //
-  //   expect(inst.toObject()).to.deep.equal({ username: 'wutwut' })
-  //   expect(inst._dirty).to.deep.equal({ username: 'wutwut' })
-  // })
-  //
-  // it('static#all()', done => {
-  //   _Module.Model.queryLog = (query, params) => {
-  //     expect(query).to.equal('select "models".* from "models" order by username DESC limit $1')
-  //     expect(params).to.deep.equal([ 11 ])
-  //
-  //     done()
-  //   }
-  //
-  //   expect(Model.all({ limit: 11, order: 'DESC', sort: 'username' })).to.be.instanceof(Promise)
-  // })
-  //
-  // it('static#one()', done => {
-  //   _Module.Model.queryLog = (query, params) => {
-  //     expect(query).to.equal('select "models".* from "models" where "models"."id" = $1 limit $2')
-  //     expect(params).to.deep.equal([ 111, 1 ])
-  //
-  //     done()
-  //   }
-  //
-  //   expect(Model.one(111)).to.be.instanceof(Promise)
-  // })
-  //
-  // it('static#find()', done => {
-  //   _Module.Model.queryLog = (query, params) => {
-  //     expect(query).to.equal('select "models".* from "models" where "models"."username" = $1 order by id ASC limit $2')
-  //     expect(params).to.deep.equal([ 'phobosman', 20 ])
-  //
-  //     done()
-  //   }
-  //
-  //   expect(Model.find({
-  //     where: { username: 'phobosman' }
-  //   })).to.be.instanceof(Promise)
-  // })
-  //
-  // it('static#count()', done => {
-  //   _Module.Model.queryLog = (query, params) => {
-  //     expect(query).to.equal('select count(*) from "models" where "models"."username" = $1')
-  //     expect(params).to.deep.equal([ 'phobosman' ])
-  //
-  //     done()
-  //   }
-  //
-  //   expect(Model.count({ username: 'phobosman' })).to.be.instanceof(Promise)
-  // })
-  //
-  // it('static#runQuery()', done => {
-  //   _Module.Model.queryLog = (query, params) => {}
-  //
-  //   const query = "select * from models WHERE models.username = 'BAM'"
-  //   const params = []
-  //
-  //   Model.runQuery(query, params, { lean: true }).then(result => {
-  //     expect(result).to.deep.equal({ result: [], count: 0 })
-  //     done()
-  //   })
-  // })
-  //
-  // it('instance#save() with new model', done => {
-  //   _Module.Model.queryLog = (query, params) => {}
-  //
-  //   const newModel = new Model({ username: 'bill' })
-  //
-  //   newModel.save().then(result => {
-  //     expect(result.username).to.equal('bill')
-  //     expect(result.id).to.be.a('number')
-  //
-  //     done()
-  //   })
-  // })
+  it('initializes a Mongo.Db object', () => {
+    expect(store).to.be.instanceof(mongo.Db)
+  })
+
+  it('model extends PhobosMongoModel', () => {
+    const inst = new Model()
+
+    expect(inst).to.be.instanceof(_Module.Model)
+    expect(inst.collection).to.equal('models')
+  })
+
+  it('properly serializes into an object via toObject() and stores changes in _dirty', () => {
+    const inst = new Model()
+
+    inst.username = 'wutwut'
+
+    expect(inst.toObject()).to.deep.equal({ username: 'wutwut' })
+    expect(inst._dirty).to.deep.equal({ username: 'wutwut' })
+  })
+
+  it('static#all()', done => {
+    _Module.Model.queryLog = query => {
+      expect(query).to.deep.equal({
+        limit: 11,
+        sort: { username: -1 },
+        skip: 0,
+        where: {},
+        type: 'find'
+      })
+
+      done()
+    }
+
+    expect(Model.all({ limit: 11, order: 'DESC', sort: 'username' })).to.be.instanceof(Promise)
+  })
+
+  it('static#one()', done => {
+    _Module.Model.queryLog = query => {
+      expect(query).to.deep.equal({
+        where: { _id: 111 },
+        type: 'findOne'
+      })
+
+      done()
+    }
+
+    expect(Model.one(111)).to.be.instanceof(Promise)
+  })
+
+  it('static#find()', done => {
+    _Module.Model.queryLog = query => {
+      expect(query).to.deep.equal({
+        limit: 20,
+        sort: { _id: 1 },
+        skip: 0,
+        where: { username: 'phobosman' },
+        type: 'find'
+      })
+
+      done()
+    }
+
+    expect(Model.find({
+      where: { username: 'phobosman' }
+    })).to.be.instanceof(Promise)
+  })
+
+  it('static#count()', done => {
+    _Module.Model.queryLog = query => {
+      expect(query).to.deep.equal({
+        where: { username: 'phobosman' },
+        type: 'count'
+      })
+
+      done()
+    }
+
+    expect(Model.count({ username: 'phobosman' })).to.be.instanceof(Promise)
+  })
+
+  it('static#runQuery()', done => {
+    _Module.Model.queryLog = query => {}
+
+    const query = {
+      limit: 20,
+      sort: { _id: 1 },
+      skip: 0,
+      where: { username: 'phobosman' },
+      type: 'find'
+    }
+
+    Model.runQuery(query, { lean: true }).then(result => {
+      expect(result.length).to.equal(0)
+      done()
+    })
+  })
+
+  it('instance#save() with new model', done => {
+    _Module.Model.queryLog = query => {}
+
+    const newModel = new Model({ username: 'bill' })
+
+    newModel.save().then(result => {
+      expect(result.username).to.equal('bill')
+      expect(result._id).to.be.instanceof(mongo.ObjectID)
+
+      done()
+    })
+  })
   //
   // it('instance#save() with existing model', done => {
   //   _Module.Model.queryLog = (query, params) => {}
